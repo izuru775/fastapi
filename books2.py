c501df2e2566127c940a29d7167b001512942df2
Isuru Adikaram <izuru775@gmail.com>
2026-07-23 21:45:03 +1000

    project 2 initialized

---
+++ books2.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class Book:
+    id:int
+    title:str
+    author:str
+    description:str
+    rating:int
+
+    def __init__(self,id:int,title:str,author:str,description:str,rating:int):
+        self.id =id
+        self.title =title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+
+BOOKS=[
+    Book(1,"Computer Science Pro","Isuru","A very nice book!",5),
+    Book(2,"Be fast with FastAPI","Isuru","A great book!",5),
+    Book(3,"Master API","Isuru","A awesome book!",5),
+    Book(4,"HP1","Author One","Book Description",2),
+    Book(5,"HP2","Author Two","Book Description",3),
+    Book(6, "HP3", "Author Three", "Book Description", 1)
+]
+@app.get("/books")
+async def get_all_books():
+    return BOOKS
