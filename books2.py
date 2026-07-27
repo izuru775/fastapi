b0f1ccffb070a02d360e8bf71651ec460b480fa9
Isuru Adikaram <izuru775@gmail.com>
2026-07-28 01:07:24 +1000

    Field validations added Function added to increment the id

--- books2.py
+++ books2.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI,Body
+from pydantic import BaseModel,Field
+
 app = FastAPI()
 
 class Book:
@@ -15,6 +17,12 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id:int| None = None
+    title:str =Field(min_length=3)
+    author:str = Field(min_length=1)
+    description:str = Field(min_length=1,max_length=100)
+    rating:int = Field(gt=-1,lt=6)
 
 
 BOOKS=[
@@ -30,6 +38,13 @@ async def get_all_books():
     return BOOKS
 
 @app.post("/create_book")
-async def create_book(book=Body()):
-    BOOKS.append(book)
+async def create_book(book:BookRequest):
+    new_book =Book(**book.model_dump())
+    print(type(new_book))
+    BOOKS.append(find_book_id(new_book))
     return BOOKS[-1]
+
+
+def find_book_id(book:Book):
+    book.id = 1 if len(BOOKS)==0 else BOOKS[-1].id +1
+    return book
