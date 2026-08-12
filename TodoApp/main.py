9e3723e8e3ab24d15b33297abc957ea972c6fcb7
Isuru Adikaram <izuru775@gmail.com>
2026-08-12 23:49:36 +1000

    first get route added to read all Todos

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from sqlalchemy.orm import Session
+from fastapi import FastAPI,Depends
 import models
+from models import Todos
 from database import engine, SessionLocal
+
 
 app = FastAPI()
 
@@ -13,3 +18,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
+@app.get("/")
+async def read_all(db:Annotated[Session,Depends(get_db)]):
+    return db.query(Todos).all()
