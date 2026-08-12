e4115c172300ff49aeff680f65884a3d356e2541
Isuru Adikaram <izuru775@gmail.com>
2026-08-12 23:30:28 +1000

    Nullable added to some columns

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -7,6 +7,6 @@ class Todos(Base):
 
     id :Mapped[int]= mapped_column(Integer,primary_key=True,index=True)
     title:Mapped[str] = mapped_column(String)
-    description:Mapped[str]= mapped_column(String)
-    priority:Mapped[int]= mapped_column(Integer)
-    complete:Mapped[bool] =mapped_column(Boolean,default=False)
+    description:Mapped[str]= mapped_column(String,nullable=True)
+    priority:Mapped[int]= mapped_column(Integer,nullable=True)
+    complete:Mapped[bool] =mapped_column(Boolean,default=False,nullable=True)
