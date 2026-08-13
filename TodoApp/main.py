1fb713c3bd9c657069cc5d95434e08f907bb1ca9
Isuru Adikaram <izuru775@gmail.com>
2026-08-14 05:41:43 +1000

    get todos by id route created and validations added

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from sqlalchemy.orm import Session
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Path
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -20,6 +20,15 @@ def get_db():
 
 db_dependency = Annotated[Session,Depends(get_db)]
 
-@app.get("/")
+
+
+@app.get("/",status_code=status.HTTP_200_OK)
 async def read_all(db:db_dependency):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}",status_code=status.HTTP_200_OK)
+async def read_todo(db:db_dependency,todo_id:int=Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404,detail="Todo not found")
