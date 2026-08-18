14cb1673423665357ad6dfe1b8c03507e39a8661
Isuru Adikaram <izuru775@gmail.com>
2026-08-18 17:17:58 +1000

    Put and Delete routes added Scalar added when calling db

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from pydantic import BaseModel, Field
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends, HTTPException, status, Path
 import models
@@ -33,7 +34,7 @@ async def read_all(db:db_dependency):
 
 @app.get("/todo/{todo_id}",status_code=status.HTTP_200_OK)
 async def read_todo(db:db_dependency,todo_id:int=Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    todo_model :Todos |None = db.scalar(select(Todos).where(Todos.id==todo_id))
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404,detail="Todo not found")
@@ -41,5 +42,36 @@ async def read_todo(db:db_dependency,todo_id:int=Path(gt=0)):
 @app.post("/todo",status_code=status.HTTP_201_CREATED)
 async def create_todo(db:db_dependency,todo_request:TodoRequest):
     todo_model:Todos = Todos(**todo_request.model_dump())
+
     db.add(todo_model)
     db.commit()
+    db.refresh(todo_model)
+
+    return {"message":"User created successfully","user_id":todo_model.id}
+
+@app.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db:db_dependency,
+                      todo_request:TodoRequest,
+                      todo_id:int= Path(gt=0)):
+    todo_model :Todos |None = db.scalar(select(Todos).where(Todos.id==todo_id))
+    if todo_model is not None:
+        todo_model.title = todo_request.title
+        todo_model.description = todo_request.description
+        todo_model.priority = todo_request.priority
+        todo_model.complete = todo_request.complete
+
+        db.add(todo_model)
+        db.commit()
+    else:
+        raise HTTPException(status_code=404,detail="Todo not found")
+
+@app.delete("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db:db_dependency,todo_id:int=Path(gt=0)):
+    todo_model:Todos |None = db.scalar(select(Todos).where(Todos.id==todo_id))
+
+    if todo_model is not None:
+        db.delete(todo_model)
+        db.commit()
+    else:
+        raise HTTPException(status_code=404,detail="Todo not found")
+
