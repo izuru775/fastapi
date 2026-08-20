178b4c79395e68397b9dadb05978e1f07bf87b76
Isuru Adikaram <izuru775@gmail.com>
2026-08-20 11:44:10 +1000

    Pydantic user request model added Create user route added

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,30 @@
-from fastapi import APIRouter
+import email
 
+from fastapi import APIRouter,status
+from pydantic import BaseModel
+from models import Users
 router = APIRouter()
 
+class CreateUserRequest(BaseModel):
+    username:str
+    email:str
+    first_name:str
+    last_name:str
+    password:str
+    role:str
 
-@router.get("/auth/")
-async def get_user():
-    return {"user":"authenticated"}
+
+@router.post("/auth",status_code=status.HTTP_201_CREATED)
+async def create_user(create_user_request:CreateUserRequest):
+    create_user_model:Users = Users(
+        username = create_user_request.username,
+        email=create_user_request.email,
+        first_name= create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        hashed_password= create_user_request.password,
+        role=create_user_request.role,
+        is_active=True
+    )
+
+
+    return create_user_model
