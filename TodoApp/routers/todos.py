3ab0017c05cf3b0813d228a5d8427da66e763dea
Isuru Adikaram <izuru775@gmail.com>
2026-08-21 00:10:48 +1000

    Auth added to post route in todos

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -4,9 +4,9 @@ from pydantic import BaseModel, Field
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status, Path
-from models import Todos
+from models import Todos, Users
 from database import SessionLocal
-
+from .auth import get_current_user
 
 router = APIRouter()
 
@@ -19,6 +19,7 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session,Depends(get_db)]
+user_dependency =Annotated[Users,Depends(get_current_user)]
 
 class TodoRequest(BaseModel):
     title:str= Field(min_length=3)
@@ -38,14 +39,18 @@ async def read_todo(db:db_dependency,todo_id:int=Path(gt=0)):
     raise HTTPException(status_code=404,detail="Todo not found")
 
 @router.post("/todo",status_code=status.HTTP_201_CREATED)
-async def create_todo(db:db_dependency,todo_request:TodoRequest):
-    todo_model:Todos = Todos(**todo_request.model_dump())
+async def create_todo(user:user_dependency,
+                      db:db_dependency,
+                      todo_request:TodoRequest):
+    if user is None:
+        raise HTTPException(status_code=401,detail="Authentication Failed")
+    todo_model = Todos(**todo_request.model_dump(),owner_id=user.id)
 
     db.add(todo_model)
     db.commit()
     db.refresh(todo_model)
 
-    return {"message":"User created successfully","user_id":todo_model.id}
+    return {"message":"Todo created successfully","todo_id":todo_model.id}
 
 @router.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(db:db_dependency,
