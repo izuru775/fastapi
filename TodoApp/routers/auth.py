7952d6b671d2129dc0590fbc19b7292745ba60ae
Isuru Adikaram <izuru775@gmail.com>
2026-08-20 14:04:01 +1000

    Saving user to DB added

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,9 +1,23 @@
-from fastapi import APIRouter,status
+from typing import Annotated
+
+from fastapi import APIRouter, status, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database import SessionLocal
 from models import Users
 from pwdlib import PasswordHash
 
 router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session,Depends(get_db)]
 
 class CreateUserRequest(BaseModel):
     username:str
@@ -19,8 +33,9 @@ def hash_password(password):
     return password_hash.hash(password)
 
 @router.post("/auth",status_code=status.HTTP_201_CREATED)
-async def create_user(create_user_request:CreateUserRequest):
-    create_user_model:Users = Users(
+async def create_user(db:db_dependency,
+                      create_user_request:CreateUserRequest):
+    user_model:Users = Users(
         username = create_user_request.username,
         email=create_user_request.email,
         first_name= create_user_request.first_name,
@@ -30,5 +45,7 @@ async def create_user(create_user_request:CreateUserRequest):
         is_active=True
     )
 
+    db.add(user_model)
+    db.commit()
 
-    return create_user_model
+    return user_model
