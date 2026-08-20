a80f7c41b5bb7e5ea117fdecf26845bec3ef457a
Isuru Adikaram <izuru775@gmail.com>
2026-08-20 13:42:06 +1000

    Password hashing implemented using pwdlib

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter,status
 from pydantic import BaseModel
 from models import Users
+from pwdlib import PasswordHash
+
 router = APIRouter()
 
 class CreateUserRequest(BaseModel):
@@ -11,6 +13,10 @@ class CreateUserRequest(BaseModel):
     password:str
     role:str
 
+password_hash =  PasswordHash.recommended()
+
+def hash_password(password):
+    return password_hash.hash(password)
 
 @router.post("/auth",status_code=status.HTTP_201_CREATED)
 async def create_user(create_user_request:CreateUserRequest):
@@ -19,7 +25,7 @@ async def create_user(create_user_request:CreateUserRequest):
         email=create_user_request.email,
         first_name= create_user_request.first_name,
         last_name=create_user_request.last_name,
-        hashed_password= create_user_request.password,
+        hashed_password= hash_password(create_user_request.password),
         role=create_user_request.role,
         is_active=True
     )
