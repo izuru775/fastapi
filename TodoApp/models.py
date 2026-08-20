7e7374d3369007bc08d0a211e83d842e7df0ecf0
Isuru Adikaram <izuru775@gmail.com>
2026-08-20 11:45:46 +1000

    User model added ForeignKey relationship created in todos table

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,6 +1,20 @@
 from database import Base
-from sqlalchemy import String,Integer,Boolean
+from sqlalchemy import String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Users(Base):
+    __tablename__="users"
+
+    id:Mapped[int]= mapped_column(Integer,primary_key=True,index=True)
+    email:Mapped[str] = mapped_column(String,unique=True)
+    username:Mapped[str] = mapped_column(String,unique=True)
+    first_name:Mapped[str]=mapped_column(String)
+    last_name:Mapped[str] = mapped_column(String)
+    hashed_password:Mapped[str]=mapped_column(String)
+    is_active:Mapped[bool] = mapped_column(Boolean,default=True)
+    role:Mapped[str]= mapped_column(String)
+
 
 class Todos(Base):
     __tablename__="todos"
@@ -10,3 +24,4 @@ class Todos(Base):
     description:Mapped[str]= mapped_column(String,nullable=True)
     priority:Mapped[int]= mapped_column(Integer,nullable=True)
     complete:Mapped[bool] =mapped_column(Boolean,default=False,nullable=True)
+    owner_id:Mapped[int] = mapped_column(Integer,ForeignKey("users.id"))
