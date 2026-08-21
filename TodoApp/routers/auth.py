bd965c9374c2733cdff8b53956031f3a0b2139f2
Isuru Adikaram <izuru775@gmail.com>
2026-08-21 11:14:10 +1000

    Bug fix

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -124,6 +124,6 @@ async def login_for_access_token(form_data:Annotated[OAuth2PasswordRequestForm,D
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub":user.username,"id":user.id},expires_delta=access_token_expires)
     return Token(access_token=access_token,token_type="bearer")
