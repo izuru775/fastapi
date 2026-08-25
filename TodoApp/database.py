a6261427dce9cb609ec8f7bf1b0154cd925a2d88
Isuru Adikaram <izuru775@gmail.com>
2026-08-25 14:28:41 +1000

    Engine updated to support postgres

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,12 +4,12 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from config import Settings,get_settings
+from config import get_settings
 
 settings = get_settings()
 SQLALCHEMY_DATABASE_URL=settings.DATABASE_URL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal =sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
