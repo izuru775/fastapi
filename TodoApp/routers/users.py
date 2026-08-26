d696e51434a42b062c1687f0dadb04bdc6bd08ab
Isuru Adikaram <izuru775@gmail.com>
2026-08-26 23:58:21 +1000

    New route added to update phone number

--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from pydantic import BaseModel, Field, ConfigDict
 from sqlalchemy import select
 from fastapi import APIRouter, Depends, HTTPException, status, Path
+from sqlalchemy.dialects.mysql import reflection
+
 from models import Users
 from .auth import get_current_user
 from .dependencies import db_dependency
@@ -36,6 +38,9 @@ class PasswordChangeRequest(BaseModel):
     password:str
     new_password:str=Field(min_length=8)
 
+class UpdatePhoneNumberRequest(BaseModel):
+    phone_number:str|None
+
 @router.get("/",status_code=status.HTTP_200_OK)
 async def get_user(user:user_dependency,db:db_dependency):
     user = db.scalar(select(Users).where(Users.id==user.id))
@@ -61,6 +66,24 @@ async def change_password(user:user_dependency,
         )
 
     user.hashed_password = hash_password(password_change_request.new_password)
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+@router.patch("/phonenumber/{phone_number}")
+async def update_phone_number(user:user_dependency,
+                              db:db_dependency,
+                              update_phone_number_request:UpdatePhoneNumberRequest
+                              ):
+    user:Users|None= db.scalar(select(Users).where(Users.id == user.id))
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    user.phone_number = update_phone_number_request.phone_number
 
     db.add(user)
     db.commit()
