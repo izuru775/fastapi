426dad1183715b1a636bd880768a98828df04bb5
Isuru Adikaram <izuru775@gmail.com>
2026-08-27 15:35:19 +1000

    Jinja2 templating added

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,11 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import models
 from database import engine, SessionLocal
 from routers import auth,todos,admin,users
-
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def test(request:Request):
+    return  templates.TemplateResponse(name="home.html", request=request)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
