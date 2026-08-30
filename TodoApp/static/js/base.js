841cefa91bb43c23b0fdfa6d2fde0c1ef0d7f227
Isuru Adikaram <izuru775@gmail.com>
2026-08-30 18:03:59 +1000

    deprecated substr method updated

--- TodoApp/static/js/base.js
+++ TodoApp/static/js/base.js
@@ -241,7 +241,7 @@
         for (let i = 0; i < cookies.length; i++) {
             const cookie = cookies[i];
             const eqPos = cookie.indexOf("=");
-            const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
             // Set the cookie's expiry date to a past date to delete it
             document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
         }
